61892c03d7ec4aae2693d36028d7618f31493732
lilzagoon <73830132+lilzagoon@users.noreply.github.com>
2021-05-03 16:23:12 +0100

    test

--- src/app/extractor.js
+++ src/app/extractor.js
@@ -225,7 +225,7 @@ export const extractData = async (files) => {
     extractedData.topDMs = extractedData.channels
         .filter((channel) => channel.isDM)
         .sort((a, b) => b.messages.length - a.messages.length)
-        .slice(0, 10);
+        .slice(0, 40);
     await Promise.all(extractedData.topDMs.map((channel) => {
         return new Promise((resolve) => {
             fetchUser(channel.dmUserID).then((userData) => {
